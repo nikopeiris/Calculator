630a67fcfb9e61673a8c5e813cba9de9346b5251
nikopeiris <nikopeiris123@gmail.com>
2025-01-08 19:23:26 +1300

    added +/-
    added a negative/ positive button.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
 let pre_ans = "";
+let decimal = false;
+let positive = true;
 
 const current_ans  = document.querySelector('.current_ans');
 const screen = document.querySelector('.screen');
@@ -28,6 +30,7 @@ function handlesymbol(symbol){
             buffer = "0";
             runningTotal = 0;
             pre_ans = "";
+            positive = true;
             break;
         
         case "=":
@@ -43,11 +46,16 @@ function handlesymbol(symbol){
 
         case "←":
             if(buffer.length === 1){
-                buffer = "0";
+                positive = true;
                 if (list_operators.includes(pre_ans.slice(-1)) && buffer === "0"){
                     pre_ans = pre_ans.substring(0, pre_ans.length - 1);
                     previousOperator = null;
                 }
+                buffer = "0";
+            }
+            else if(buffer.slice(-1) === "."){
+                decimal = false;
+                buffer = buffer.substring(0, buffer.length - 1);
             }
             else{
                 buffer = buffer.substring(0, buffer.length - 1);
@@ -62,7 +70,17 @@ function handlesymbol(symbol){
             break;
 
         case ".":
-            buffer += ".";
+            if (decimal){
+                return;
+            }
+            else{
+                buffer += ".";
+                decimal = true;
+            }
+            break;
+
+        case "+/-":
+            handlenegative();
             break;
     }
 }
@@ -88,6 +106,7 @@ function handleMath(symbol){
     previousOperator = symbol;
     pre_ans = runningTotal + symbol;
     buffer = "0";
+    positive = true;
 }
 
 function flushOperation(intBuffer){
@@ -119,6 +138,19 @@ function handlenumber(numberString){
     }
     else if(buffer !== "0" && pre_ans === ""){
         buffer += numberString;
+    }
+}
+
+function handlenegative(){
+    if (buffer !== "0") {
+        if (positive) {
+            buffer = "-" + buffer;
+            positive = false;
+        }
+        else{
+            buffer = buffer.substring(1, buffer.length);
+            positive = true;
+        }
     }
 }
 
